d0db3c0aea9628489b835d4c7b840ce45f5bd179
lijixiang <820396305@qq.com>
2017-06-19 10:51:36 +0800

    service/index.js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 ## Usage
 
-This is a project template for [vue-cli](https://github.com/vuejs/vue-cli). **It is recommended to use npm 3+ for a more efficient dependency tree.**
+This is a project template for [vue-cli](https://github.com/vuejs/vue-cli).
 
 ``` bash
 $ npm install -g vue-cli
--- template/src/service/index.js
+++ template/src/service/index.js
@@ -1,18 +1,12 @@
 import axios from 'axios'
-import { urlQueryString } from 'utils/index'
 
 export const queryData = (method, url) => {
-  return params => new Promise((resolve, reject) => {
-    if (method.toLowerCase() === 'get') {
-      url = urlQueryString(url, params)
-      params = {}
+  return (params = {}) => new Promise((resolve, reject) => {
+    if (['get', 'delete'].indexOf(method.toLowerCase()) > -1) {
+      params = { params }
     }
-    return axios[method](url, params).then(rs => {
-      if (rs.data.status === 200) {
-        resolve(rs.data.data)
-      } else {
-        reject(rs.data.message)
-      }
+    return axios[method](url, params).then(({data}) => {
+      data.status === 200 ? resolve(data.data) : reject(data.message)
     }).catch(() => {
       reject('通讯异常')
     })
