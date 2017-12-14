c5c27d73d9ea40d06aabcffa8b19b6d2858bcd14
mjixiang <820396305@qq.com>
2017-12-14 17:47:10 +0800

    s

--- template/src/views/demo/index.vue
+++ template/src/views/demo/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h2>当前环境</h2>
-    <div>是否ios设备：{{$isIos}}</div>
-    <div>是否微信环境：{{$isWechat}}</div>
+    <div>是否ios设备：<span v-text="$isIos"></span></div>
+    <div>是否微信环境：<span v-text="$isWechat"></span></div>
   </div>
 </template>
 
